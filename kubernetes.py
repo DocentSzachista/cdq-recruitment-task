bf69b73424a6e2905772687c3a3857aeb9bbe6f9
Damian Raczkowski <57325417+DocentSzachista@users.noreply.github.com>
2025-11-08 11:55:13 +0100

    Feat/script (#1) Add python script to generate k8s deployment
    * feat: :sparkles: Add initial version of script to generate k8s deployment
    
    Adds app code which accepts parameters to declare fields for kubernetes deployment.
    
    * refactor: :art: Improve manifest base class to better fit DRY principle
    
    Added default behaviour and logic to methods for Manifest class so it could be easier extended if more objects were to be added
    
    * fix: :bug: Add required dependencies to requirements.txt file
    
    * chore: Add gitignore file

---
+++ kubernetes.py
@@ -0,0 +1,33 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+class Manifest:
+    """Base class for kubernetes manifest generation program"""
+
+    dirpath = "manifests"
+    template = str()
+    required_args = set()
+
+    def __init__(self, args: dict):
+        if type(self) is Manifest:
+            raise TypeError("Cannot instantiate Manifest directly, use a subclass instead")
+        self._environment = Environment(loader=FileSystemLoader(self.dirpath))
+        self._args = args
+        self._check_args()
+
+    def _check_args(self):
+        """Valides if all of the required arguments for the manifest has been supplied"""
+        missing_args = self.required_args - self._args.keys()
+        if missing_args:
+            raise ValueError(f"Required arguments are missing: {", ".join(missing_args)}")
+
+    def produce_manifest(self):
+        """Generates kubernetes manifest from the file placed in manifests directory."""
+        template = self._environment.get_template(self.template)
+        return template.render(**self._args)
+
+
+class Deployment(Manifest):
+
+    template = "deployment.yaml.j2"
+    required_args = {"name"}
